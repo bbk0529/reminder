c1ac24e4b3ebe557fde059ee5494846730391296
bbk0529 <bbk0529@gmail.com>
2019-10-03 20:55:22 +0200

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.conf.urls import include, url
 
 urlpatterns = [
-    url(r'^blog/', include('blog.urls')),
+    path(r'^blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
 ]
