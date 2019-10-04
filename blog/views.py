70d18f3e4f9c45e25b0005f141390a57c84de2b3
bk <bbk0529@gmail.com>
2019-10-04 18:27:22 +0200

    dollar added

--- blog/views.py
+++ blog/views.py
@@ -11,7 +11,7 @@ def keyboard(request):
 
     return JsonResponse({
         'type':'buttons',
-        'buttons':['오늘','내일', 'Exchange']
+        'buttons':['Euro','Dollar']
     })
 
 @csrf_exempt
@@ -21,36 +21,26 @@ def answer(request):
     received_json_data = json.loads(json_str)
     datacontent = received_json_data['content']
 
-    if datacontent == '오늘':
-        today = "오늘 급식"
-
-        return JsonResponse({
-                'message': {
-                    'text': today
-                },
-                'keyboard': {
-                    'type':'buttons',
-                    'buttons':['오늘','내일', 'Exchange']
-                }
-
-            })
-
-    elif datacontent == '내일':
-        tomorrow = "내일 급식"
-
-        return JsonResponse({
-                'message': {
-                    'text': tomorrow
-                },
-                'keyboard': {
-                    'type':'buttons',
-                    'buttons':['오늘','내일', 'Exchange']
-                }
-
-            })
-
-    elif datacontent =='Exchange':
+    if datacontent == 'Euro':
         link="https://www.google.de/search?q=1euro+to+krw&oq=1euro+to+krw"
+        page=requests.get(link)
+        soup=BeautifulSoup(page.content, 'html.parser')
+        result=soup.find(class_="BNeawe iBp4i AP7Wnd").get_text()
+
+        return JsonResponse({
+                'message': {
+                    'text': result
+                },
+                'keyboard': {
+                    'type':'buttons',
+                    'buttons':['Euro','Dollar']
+                }
+
+            })
+
+
+    elif datacontent =='Dollar':
+        link="https://www.google.de/search?q=1dollar+to+krw&oq=1dollar+to+krw"
         page=requests.get(link)
         soup=BeautifulSoup(page.content, 'html.parser')
         result=soup.find(class_="BNeawe iBp4i AP7Wnd").get_text()
@@ -62,7 +52,7 @@ def answer(request):
                 },
                 'keyboard': {
                     'type':'buttons',
-                    'buttons':['오늘','내일', 'Exchange']
+                    'buttons':['Euro','Dollar']
                 }
 
             })
